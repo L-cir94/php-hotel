3651be538987e3994c888dfccb2377955f436698
L-cir94 <luca.cirigliano94@hotmail.it>
2023-05-05 16:07:51 +0200

    hotels in table with html e php

--- index.php
+++ index.php
@@ -56,6 +56,7 @@ $hotels = [
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -63,53 +64,50 @@ $hotels = [
     <link defer href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>php-hotel</title>
 </head>
+
 <body>
-<header class="bg-dark text-white">
-    <div class="container">
-        <div class="row">
-        <h1>Hotels</h1>
-        </div>
-    </div>
-</header>
-<main>
-    <div class="jumbotron">
-        <h1 class="display-3">Welcome to our hotels</h1>
-        <p class="lead">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae quas perferendis veniam, eius dolorum sapiente tempore hic, voluptatem, minima aliquid nulla. Ipsam, provident! Fugit tempore quam sunt consequuntur rem debitis!</p>
-        <hr class="my-2">
-        <?php foreach ($hotels as $key => $value){
-    echo "<strong>$key</strong> ";
-    foreach ($value as $key => $value) {
-        echo "<p><strong>$key</strong>: $value</p>";
-    }
-};?>
-        <section class="hotels">
-            <div class="table-responsive">
-                <table class="table table-primary">
-                    <thead>
-                        <tr>
-                            <th scope="col">Descrizione Hotel</th>
-                            <th scope="col">Parcheggio</th>
-                            <th scope="col">voto</th>
-                            <th scope="col">Distanza dal centro abitato</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr class="">
-                            <td scope="row">R1C1</td>
-                            <td>R1C2</td>
-                            <td>R1C3</td>
-                        </tr>
-                        <tr class="">
-                            <td scope="row">Item</td>
-                            <td>Item</td>
-                            <td>Item</td>
-                        </tr>
-                    </tbody>
-                </table>
+    <header class="bg-dark text-white">
+        <div class="container">
+            <div class="row">
+                <h1>Hotels</h1>
             </div>
-            
-        </section>    
-    </div>
-</main>
+        </div>
+    </header>
+    <main>
+        <div class="jumbotron">
+            <h1 class="display-3">Welcome to our hotels</h1>
+            <p class="lead">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae quas perferendis veniam, eius dolorum sapiente tempore hic, voluptatem, minima aliquid nulla. Ipsam, provident! Fugit tempore quam sunt consequuntur rem debitis!</p>
+            <hr class="my-2">
+            <section class="hotels">
+                <div class="container">
+                    <div class="table-responsive">
+                        <table class="table table-primary">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Hotel</th>
+                                    <th scope="col">Descrizione</th>
+                                    <th scope="col">Parcheggio</th>
+                                    <th scope="col">Voto</th>
+                                    <th scope="col">Distanza dal centro abitato</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($hotels as $key => $value) : ?>
+                                    <tr class="">
+                                        <td scope="row"><?= $value['name'] ?></td>
+                                        <td><?= $value['description'] ?></td>
+                                        <td><?= $value['parking'] ?></td>
+                                        <td><?= $value['vote'] ?></td>
+                                        <td><?= $value['distance_to_center'] ?></td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </main>
 </body>
+
 </html>
